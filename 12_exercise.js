91a6a9233e62efc7b9ef4a22039712c19122b14b
Mickell <mickellc@hotmail.co.uk>
2018-03-16 10:03:19 +0000

    Create variables to check if avaliable immediately or fresh grad and filter function to check the options

--- 12_exercise.js
+++ 12_exercise.js
@@ -16,47 +16,67 @@
  *   happy refactory :)
  */
 
-function filter(candidates, filters) {
-  let filteredCandidates = [];
+function filter(candidates, filters = []) {
+  // let filteredCandidates = [];
+  let candidatesLength = candidates.length;
+  let filterLength = filters.length;
   let profile;
-  let availableImmediatelyFilter = filters.includes("AVAILABLE_IMMEDIATELY");
-  let freshGradFilter = !availableImmediatelyFilter && filters.includes("FRESH_GRAD");
 
-  if (filters.length === 0) {
-    return candidates;
+  if (!filterLength ) {
+    return candidates
   }
 
-  for (let i = candidates.length; i--; ) {
-    profile = candidates[i].profiles && candidates[i].profiles.length > 0; //has.profiles
+  availableImmediatelyFilter = filters.includes('AVAILABLE_IMMEDIATELY');
+  freshGradFilter = !availableImmediatelyFilter && filters.includes('FRESH_GRAD');
 
-    if (candidates[i].profiles) {
-      for (let k = filters.length; k--; ) {
-        // loop through filters
-        let hasFilter = false;
-        for (let j = candidates[i].profiles.length; j--; ) {
-          if (!availableImmediatelyFilter && !freshGradFilter) {
-            if (filters[k] == candidates[i].profiles[j].code) {
-              hasFilter = true;
-            }
-          } else if (
-            availableImmediatelyFilter &&
-            candidates[i].profiles[j].code === "AVAILABLE_IMMEDIATELY"
-          ) {
-            hasFilter = true;
-          } else if (
-            freshGradFilter &&
-            candidates[i].profiles[j].code === "FRESH_GRAD"
-          ) {
-            hasFilter = true;
-          }
-        }
-        profile = profile && hasFilter;
-      }
+  const checkCandidate = (profiles = [], filter) => profiles.some(profiles => filter.includes(profiles.code));
+
+  return candidates.filter(({profiles = []}) => {
+    if(availableImmediatelyFilter) {
+      return checkCandidate(profiles, 'AVAILABLE_IMMEDIATELY');
     }
-    if (profile) {
-      filteredCandidates.unshift(candidates[i]);
+
+    if(freshGradFilter) {
+      return checkCandidate(profiles, 'FRESH_GRAD');
     }
-  }
+
+    return profiles;
+  })
+
+  filters.some(candidates => candidates)
+
+
+    // for (let i = candidatesLength; i--; ) {
+    //   profile = candidates[i].profiles && candidates[i].profiles.length > 0; //has.profiles
+    //
+    //   if (candidates[i].profiles) {
+    //     for (let k = filterLength; k--; ) {
+    //       // loop through filters
+    //       let hasFilter = false;
+    //       for (let j = candidates[i].profiles.length; j--; ) {
+    //         if (!availableImmediatelyFilter && !freshGradFilter) {
+    //           if (filters[k] == candidates[i].profiles[j].code) {
+    //             hasFilter = true;
+    //           }
+    //         } else if (
+    //           availableImmediatelyFilter &&
+    //           candidates[i].profiles[j].code === 'AVAILABLE_IMMEDIATELY'
+    //         ) {
+    //           hasFilter = true;
+    //         } else if (
+    //           freshGradFilter &&
+    //           candidates[i].profiles[j].code === 'FRESH_GRAD'
+    //         ) {
+    //           hasFilter = true;
+    //         }
+    //       }
+    //       profile = profile && hasFilter;
+    //     }
+    //   }
+    //   if (profile) {
+    //     filteredCandidates.unshift(candidates[i]);
+    //   }
+    // }
   return filteredCandidates;
 }
 
